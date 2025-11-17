0ef3fabb2c9fde49aa4b83e0e28585f31d5a92b2
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2025-11-16 16:39:08 -0800

    Adding in interpreter file + skeleton for operators

--- postscript_interpreter/interpreter.go
+++ postscript_interpreter/interpreter.go
@@ -1,1 +1,131 @@
 package main
+
+import (
+	"fmt"
+)
+
+type Interpreter struct {
+	opStack     *Stack                              // operand stack
+	dictStack   []*PSDict                           // stack of dictionaries
+	lexicalMode bool                                // for dynamic/lexical scoping
+	operators   map[string]func(*Interpreter) error // map of operators and values
+}
+
+// function acting like a constructor
+func CreateInterpreter() *Interpreter {
+	globalDict := make(PSDict)
+
+	interpreter := &Interpreter{
+		opStack:     CreateStack(),
+		dictStack:   []*PSDict{&globalDict},
+		lexicalMode: false,
+		operators:   make(map[string]func(*Interpreter) error),
+	}
+
+	interpreter.registerOperators()
+	return interpreter
+}
+
+// creating dictionary of register operators and their associated functions
+func (i *Interpreter) registerOperators() {
+
+	i.operators["add"] = opAdd
+	i.operators["sub"] = opSub
+	i.operators["mul"] = opMul
+	i.operators["dup"] = opDup
+	i.operators["pop"] = opPop
+	i.operators["exch"] = opExch
+}
+
+func (i *Interpreter) Execute(tokens []Token) error {
+	pos := 0
+	if pos < len(tokens) {
+		token := tokens[pos]
+		switch token.Type {
+		// if it's a value type, push it onto the stack
+		case TOKEN_BOOL:
+			i.opStack.Push(token.Value)
+
+		case TOKEN_INT:
+			i.opStack.Push(token.Value)
+
+		case TOKEN_FLOAT:
+			i.opStack.Push(token.Value)
+
+		case TOKEN_STRING:
+			i.opStack.Push(token.Value)
+
+		case TOKEN_NAME:
+			val := token.Value
+			name := PSName(val.(string))
+			i.opStack.Push(name)
+
+		// if it's an operator type, search for it in the dictionary
+		case TOKEN_OPERATOR:
+			val := token.Value.(string)
+			opFunc, ok := i.operators[val]
+			if !ok {
+				return fmt.Errorf("operator not defined", val)
+			}
+			err := opFunc(i)
+			if err != nil {
+				return err
+			}
+
+		// if it's the start of a code block
+		case TOKEN_BLOCK_START:
+			proc, newPos, err := i.buildProcedure(tokens, pos)
+			if err != nil {
+				return err
+			}
+
+			i.opStack.Push(proc)
+			pos = newPos
+
+		// this shouldn't happen but if it hits the end of a code block
+		case TOKEN_BLOCK_END:
+			return fmt.Errorf("how'd you get here")
+		}
+
+		pos++
+	}
+
+	return nil
+}
+
+func (i *Interpreter) buildProcedure(tokens []Token, startPos int) (PSBlock, int, error) {
+	blockTokens := []Token{}
+	depthCounter := 1
+	pos := startPos + 1
+
+	for depthCounter > 0 && pos < len(tokens) {
+		currentToken := tokens[pos]
+
+		if currentToken.Type == TOKEN_BLOCK_START {
+			depthCounter++
+		}
+
+		if currentToken.Type == TOKEN_BLOCK_END {
+			depthCounter--
+			if depthCounter == 0 {
+				pos++
+				break
+			}
+		}
+
+		if depthCounter > 0 {
+			blockTokens = append(blockTokens, currentToken)
+		}
+		pos++
+	}
+
+	if depthCounter > 0 {
+		return PSBlock{}, pos, fmt.Errorf("unclosed procedure")
+	}
+
+	proc := PSBlock{
+		Body: blockTokens,
+	}
+
+	return proc, pos, nil
+}
--- postscript_interpreter/operators.go
+++ postscript_interpreter/operators.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func opAdd(i *Interpreter) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func opSub(i *Interpreter) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func opMul(i *Interpreter) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func opDup(i *Interpreter) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func opPop(i *Interpreter) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func opExch(i *Interpreter) error {
+	return fmt.Errorf("Not implemented")
+}
